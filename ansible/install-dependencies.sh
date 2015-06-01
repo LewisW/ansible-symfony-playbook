a305d67ce461fc90d4f7dc596f0759dc7b2218bd
Lewis Wright <LewisW@users.noreply.github.com>
2015-06-01 13:50:46 +0100

    Changed roles directory to be optional with first argument

--- ansible/install-dependencies.sh
+++ ansible/install-dependencies.sh
@@ -2,18 +2,16 @@
 cd $(dirname $0)
 
 if ! hash ansible-playbook 2>/dev/null; then
-
-	#if hash apt-get 2>/dev/null; then
-	#	sudo apt-get update
-	#	sudo apt-get install -y software-properties-common
-    #    sudo apt-add-repository -y ppa:ansible/ansible
-    #    sudo apt-get update
-    #    sudo apt-get install -y ansible
-	#else
-		sudo easy_install pip
-		sudo -H pip install ansible markupsafe --allow-external mysql-connector-python mysql-connector-python --quiet
-		sudo -H pip install mysql-python boto
-	#fi
+	sudo easy_install pip
+	sudo -H pip install ansible markupsafe --allow-external mysql-connector-python mysql-connector-python --quiet
+	sudo -H pip install mysql-python boto
 fi
 
-sudo ansible-galaxy install -r requirements.txt --ignore-errors --force -p ./roles/
+if [ "$1" != "" ];
+then
+	ROLES="-p $1"
+else
+	ROLES=
+fi
+
+sudo ansible-galaxy install -r requirements.txt --ignore-errors --force $ROLES
